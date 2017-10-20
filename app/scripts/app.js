b62381e7a382da17b1f3b7cce025bfb7ea30eb42
Jim Knopf <jsk4@mac.com>
2017-10-20 10:37:19 -0700

    Checkpoint 3 - Routing and States completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,38 @@
-angular.module('blocJams', []);
+// angular.module('blocJams', []);
+// dependency injection-- th empty array (2nd argumet above) is list of external modules app depends on
+// angular.module('blocJams', ['ui.router']);
+
+
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider    // configure paths - make url look nice, not use #! hashbang
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+      // use $stateProvider so app can configure name,url,controller and template
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html',
+      // });   remove ; to start chaining calls
+
+        controller: 'LandingCtrl as landing'
+
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
+      });
+  }
+
+  // angular.module('blocJams', ['ui-router']);
+    angular
+      .module('blocJams', ['ui.router'])
+      .config(config);
+})();
