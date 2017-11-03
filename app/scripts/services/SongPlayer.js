b1c20269b5be4a976576e17c16fffd11e86a2ca5
Jim Knopf <jsk4@mac.com>
2017-11-02 22:58:06 -0700

    Checkpoint 7 Services Part2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,8 +3,18 @@
          var SongPlayer = {};
 
          var currentSong = null;
+
+         /**
+         * @desc Buzz object audio file
+         * @type {Object}
+         */
          var currentBuzzObject = null;
 
+         /**
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
          var setSong = function(song) {
            if (currentBuzzObject) {
               currentBuzzObject.stop();
@@ -16,30 +26,32 @@
            preload: true
          });
 
-    currentSong = song;
+         currentSong = song;
  };
 
          SongPlayer.play = function(song) {
            if (currentSong !== song) {
-               if (currentBuzzObject) {
-                   currentBuzzObject.stop();
-                   currentSong.playing = null;
+              //  if (currentBuzzObject) {
+              //      currentBuzzObject.stop();
+              //      currentSong.playing = null;
+              //    }
+               //
+              //    currentBuzzObject = new buzz.sound(song.audioUrl, {
+              //       formats: ['mp3'],
+              //       preload: true
+              //    });
+               //
+
+                // currentSong = song;
+                setSong(song);
+                currentBuzzObject.play();
+                song.playing = true;
+
                } else if (currentSong === song) {
                   if (currentBuzzObject.isPaused()) {
                       currentBuzzObject.play();
                   }
                }
-               // var currentBuzzObject = new buzz.sound(song.audioUrl, {
-               currentBuzzObject = new buzz.sound(song.audioUrl, {
-                 formats: ['mp3'],
-                 preload: true
-               });
-
-               currentSong = song;
-
-               currentBuzzObject.play();
-               song.playing = true;
-             }
           };
 
           SongPlayer.pause = function(song) {
