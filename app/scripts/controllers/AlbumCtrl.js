dd4fcb31eb0cada3805a0b1c15d08af0e07695d2
Jim Knopf <jsk4@mac.com>
2017-11-02 21:42:43 -0700

    Checkpoint 7: 1/2 way done-- ready to refactor

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,15 +1,21 @@
 (function() {
     // function AlbumCtrl() {
     // inject custom service Fixtures into AlbumCtrl
-    function AlbumCtrl(Fixtures) {
+    // function AlbumCtrl(Fixtures) {
       // this.albumData = albumPicasso;
       //above appears to work, but use below???
       // this.albumData = angular.copy(albumPicasso);
       // now moot anyway bc will use Fixtures service's getAlbum() method
-      this.albumData = Fixtures.getAlbum();
-    }
+      // this.albumData = Fixtures.getAlbum();
+
+      // inject the song player service
+      function AlbumCtrl(Fixtures, SongPlayer) {
+        this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
+      }
 
     angular
         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        //  .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,56 @@
+(function() {
+    function SongPlayer() {
+         var SongPlayer = {};
+
+         var currentSong = null;
+         var currentBuzzObject = null;
+
+         var setSong = function(song) {
+           if (currentBuzzObject) {
+              currentBuzzObject.stop();
+              currentSong.playing = null;
+            }
+
+         currentBuzzObject = new buzz.sound(song.audioUrl, {
+           formats: ['mp3'],
+           preload: true
+         });
+
+    currentSong = song;
+ };
+
+         SongPlayer.play = function(song) {
+           if (currentSong !== song) {
+               if (currentBuzzObject) {
+                   currentBuzzObject.stop();
+                   currentSong.playing = null;
+               } else if (currentSong === song) {
+                  if (currentBuzzObject.isPaused()) {
+                      currentBuzzObject.play();
+                  }
+               }
+               // var currentBuzzObject = new buzz.sound(song.audioUrl, {
+               currentBuzzObject = new buzz.sound(song.audioUrl, {
+                 formats: ['mp3'],
+                 preload: true
+               });
+
+               currentSong = song;
+
+               currentBuzzObject.play();
+               song.playing = true;
+             }
+          };
+
+          SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+          };
+
+        return SongPlayer;
+    }
+
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
